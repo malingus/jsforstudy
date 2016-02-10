da0e82bab63f7760e26e62163afe7be4591c21ec
malingus <marajke18@gmail.com>
2016-02-10 16:05:18 +0200

    an example of method

---
+++ literal_method.js
@@ -0,0 +1,11 @@
+var james = {
+    job: "programmer",
+    married: false,
+    speak: function(mood) {
+        console.log("Hello, I am feeling "+mood);
+        
+    }
+};
+
+james.speak("great");
+james.speak("just okay");
