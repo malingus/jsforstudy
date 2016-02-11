a662cb5c905afc873f98ff6815bc17686afe2ec6
malingus <marajke18@gmail.com>
2016-02-11 09:34:51 +0200

    an exemple of typeof identificator

---
+++ typeof.js
@@ -0,0 +1,7 @@
+var anObj = { job: "I'm an object!" };
+var aNumber = 42;
+var aString = "I'm a string!";
+
+console.log( typeof anObj ); 
+console.log( typeof aNumber ); 
+console.log( typeof aString ); 
